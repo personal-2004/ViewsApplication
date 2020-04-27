1b04cf5a4f2b038da49670d8a69c45bd054f7ac8
personal-2004 <al_sy@ukr.net>
2020-04-27 17:10:19 +0300

    init commit_27.04.20

---
+++ app/src/main/java/com/example/viewsapplication/ConstraintExample.java
@@ -0,0 +1,14 @@
+package com.example.viewsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_example);
+    }
+}
