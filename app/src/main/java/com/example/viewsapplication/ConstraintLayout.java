b03974e40fe1e562a9e9ae113b6a605291484f75
personal-2004 <al_sy@ukr.net>
2020-05-07 17:15:39 +0300

    init commit_07.05.20

--- app/src/main/java/com/example/viewsapplication/ConstraintLayout.java
+++ app/src/main/java/com/example/viewsapplication/ConstraintLayout.java
@@ -2,13 +2,25 @@ package com.example.viewsapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.content.Intent; // подключаем класс Intent
+import android.view.Gravity;
+import android.view.View; // подключаем класс View для обработки нажатия кнопки
+import android.widget.EditText; // подключаем класс EditText
 
+@SuppressLint("Registered")
 public class ConstraintLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint_layout);
+    }
+    // Метод обработки нажатия на кнопку
+
+    public void sendMessageActivityImage (View view){
+        Intent intentActivityImage = new Intent(ConstraintLayout.this,ActivityImage.class);
+        startActivity(intentActivityImage);
     }
 }
